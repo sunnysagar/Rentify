76f202bf115b4112701ead403a3ebc408a930598
Sunny <sunnysagar620@gmail.com>
2025-01-18 12:14:09 +0530

    commit the changes of env

--- src/main/java/com/sunny/Rentify/RentifyApplication.java
+++ src/main/java/com/sunny/Rentify/RentifyApplication.java
@@ -1,5 +1,6 @@
 package com.sunny.Rentify;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RentifyApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		// Set environment variables manually if needed
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(RentifyApplication.class, args);
 	}
 
